02dfe1ab72f2ae5b503bc1f278928e562a113696
Luay Abbas <abbasluay1981@gmail.com>
2023-07-27 19:50:52 +0200

    change-colors

---
+++ index.js
@@ -0,0 +1,23 @@
+const box = document.querySelector(".box >div")
+const spans = document.querySelectorAll(".box>div>span")
+
+
+
+let colorsArray=["#9a27aec4","#ff8c00d1","#86f604de","#ffe500de","#ff1100de","#00e2ffe6"]
+let previous = -1
+
+setInterval(()=>{
+    spans.forEach( item=> {
+
+        let randomColor 
+        do{
+            randomColor = Math.floor(Math.random()* colorsArray.length )
+            item.style.background= colorsArray[randomColor]}while(previous ===randomColor)
+        console.log(randomColor)
+        previous = randomColor
+       
+        })
+    
+},4000)
+
+   
