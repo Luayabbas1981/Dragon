67862cda7d520d63dcc1982fdc2a03976f2a4a13
Luay Abbas <abbasluay1981@gmail.com>
2023-07-28 00:30:16 +0200

    change-colors

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ let colorsArray = [
   "#00e2ffe6",
 ];
 
-
+let index = 0
 
 setInterval(() => {
   let newArray=  colorsArray.sort(() => Math.random() - 0.5).slice(0,4);
   spans.forEach((item,i) => {
     item.style.background = newArray[i];
     item.setAttribute("id",newArray[i])
-    box.style = `--before:${newArray[i]}`;
   });
+  setInterval(() => {
+    box.style = `--before:${spans[index].id}`;
+    index++
+    console.log(index)
+   if(index ===3){
+    index = 0
+   }
+}, 500);
 }, 4000);
