2f88ce735afa6e5c960902318c00a56953649547
Luay Abbas <abbasluay1981@gmail.com>
2023-07-28 09:13:52 +0200

    change-colors

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const box = document.querySelector(".box");
 const spans = document.querySelectorAll(".box>div>span");
-
+let originalColors = [
+  "#fc7e6b",
+  " #fa53db",
+  "#7890fd",
+  "#ff8c00",
+  "#fc7e6b",
+  " #fa53db",
+  "#7890fd",
+  "#ff8c00",
+];
 let colorsArray = [
   "#9a27aec4",
   "#ff8c00d1",
@@ -8,25 +17,31 @@ let colorsArray = [
   "#ffe500de",
   "#ff1100de",
   "#00e2ffe6",
+  "#0228fddb",
+  "#d802fddb",
 ];
 
-let index = 0
-
+let originalArrayIndex = 0;
+let index = 0;
+let newArray;
 setInterval(() => {
-  let newArray=  colorsArray.sort(() => Math.random() - 0.5).slice(0,4);
-  
-  spans.forEach((item,i) => {
+  newArray = colorsArray.sort(() => Math.random() - 0.5).slice(0, 4);
+
+  spans.forEach((item, i) => {
     item.style.background = newArray[i];
-    item.setAttribute("id",newArray[i])
+    item.setAttribute("id", newArray[i]);
   });
- 
 }, 4000);
 
 setInterval(() => {
+  if (newArray === undefined) {
+    box.style = `--before:${originalColors[originalArrayIndex]}`;
+    originalArrayIndex++;
+  } else {
     box.style = `--before:${spans[index].id}`;
-    index++
-    console.log(index)
-   if(index ===3){
-    index = 0
-   }
+    index++;
+    if (index === 3) {
+      index = 0;
+    }
+  }
 }, 500);
